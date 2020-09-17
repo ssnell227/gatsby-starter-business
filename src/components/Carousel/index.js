c1b4229f4d6ff914bb7e97b06a7d6f67003e5c18
ssnell227 <ssnell227@gmail.com>
2020-09-16 18:15:02 -0600

    finishes carousel animations

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -4,6 +4,8 @@ import './styles.scss'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import SearchIcon from '@material-ui/icons/Search';
 import leftArrow from '../../../static/icons/arrow-left-thick.png'
 import rightArrow from '../../../static/icons/arrow-right-thick.png'
 
@@ -47,18 +49,26 @@ const Carousel = ({ content }) => {
             <div className='column mobile-is-12 is-10 is-offset-1'>
                 <Slider {...settings}>
                     <div className='slider-item'>
-                        <h2 className='carousel-title'>{content.carousel1.title}</h2>
-                        <p className='carousel-text'>{content.carousel1.text}</p>
+                        <h2 className=
+                            'has-text-weight-bold is-size-4 is-size-2-tablet is-size-1-desktop carousel-text title-1'
+                        >{content.carousel1.title}</h2>
+                        <p className=' has-text-weight-bold is-size-6 is-size-4-tablet carousel-text text-1 '>{content.carousel1.text}</p>
                         <img src={content.carousel1.image} />
                     </div>
                     <div className='slider-item'>
-                        <h2 className='carousel-title'>{content.carousel2.title}</h2>
-                        <p className='carousel-text'>{content.carousel2.text}</p>
+                        <div className='carousel-icon icon-2'>
+                            <SearchIcon />
+                        </div>
+                        <h2 className='has-text-weight-bold has-text-right is-size-4 is-size-2-tablet is-size-1-desktop carousel-text title-2'>{content.carousel2.title}</h2>
+                        <p className='carousel-text has-text-right has-text-weight-bold is-size-6 is-size-5-tablet is-size-4-desktop text-2'>{content.carousel2.text}</p>
                         <img src={content.carousel2.image} />
                     </div>
                     <div className='slider-item'>
-                        <h2 className='carousel-title'>{content.carousel3.title}</h2>
-                        <p className='carousel-text'>{content.carousel3.text}</p>
+                        <div className='carousel-icon icon-3'>
+                            <MenuBookIcon/>
+                        </div>
+                        <h2 className='has-text-weight-bold is-size-4 is-size-2-tablet is-size-1-desktop carousel-text title-3'>{content.carousel3.title}</h2>
+                        <p className='has-text-weight-bold is-size-6 is-size-4-tablet carousel-text text-3'>{content.carousel3.text}</p>
                         <img src={content.carousel3.image} />
                     </div>
                 </Slider>
